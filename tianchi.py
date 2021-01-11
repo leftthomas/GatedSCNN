b5b41908e0eb0242414b0da94b428c1c3076b705
leftthomas <leftthomas@qq.com>
2021-01-11 17:37:59 +0800

    add val data

--- tianchi.py
+++ tianchi.py
@@ -1,5 +1,7 @@
 import argparse
 import json
+import random
+import shutil
 
 from utils.datasets import create_folder
 
@@ -8,11 +10,13 @@ if __name__ == '__main__':
     parser.add_argument('--label_file', type=str, default='data/annotations/train_annos.json', help='labels file path')
     opt = parser.parse_args()
     with open(opt.label_file, 'r') as load_f:
-        load_dict = json.load(load_f)
-    path = 'data/train/labels'
-    create_folder(path)
-    for item in load_dict:
-        file_name = '{}/{}'.format(path, item['name'].replace('jpg', 'txt'))
+        train_annos = json.load(load_f)
+    train_path, val_path = 'data/train', 'data/val'
+    create_folder('{}/labels'.format(train_path))
+    create_folder('{}/labels'.format(val_path))
+    create_folder('{}/images'.format(val_path))
+    for item in train_annos:
+        file_name = '{}/labels/{}'.format(train_path, item['name'].replace('jpg', 'txt'))
         file = open(file_name, mode='a')
         tx, ty, bx, by = item['bbox']
         w, h = item['image_width'], item['image_height']
@@ -20,3 +24,15 @@ if __name__ == '__main__':
         bw, bh = bx - tx, by - ty
         file.write('{} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(item['category'], cx / w, cy / h, bw / w, bh / h))
         file.close()
+    # random select 388 images as val data
+    val_annos = random.sample(train_annos, 388)
+    for item in val_annos:
+        file_name = '{}/labels/{}'.format(val_path, item['name'].replace('jpg', 'txt'))
+        file = open(file_name, mode='a')
+        tx, ty, bx, by = item['bbox']
+        w, h = item['image_width'], item['image_height']
+        cx, cy = (tx + bx) / 2, (ty + by) / 2
+        bw, bh = bx - tx, by - ty
+        file.write('{} {:.6f} {:.6f} {:.6f} {:.6f}\n'.format(item['category'], cx / w, cy / h, bw / w, bh / h))
+        file.close()
+        shutil.copy('{}/images/{}'.format(train_path, item['name']), '{}/images/{}'.format(val_path, item['name']))
