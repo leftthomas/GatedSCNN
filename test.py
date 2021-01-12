7f7e0e03c5321c7b7a75db26056563645ed084a6
leftthomas <leftthomas@qq.com>
2021-01-12 13:17:05 +0800

    fix bug

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import argparse
-import glob
 import json
 import os
-import shutil
 from pathlib import Path
 
 import numpy as np
@@ -11,14 +9,14 @@ import yaml
 from tqdm import tqdm
 
 from utils.datasets import create_dataloader
-from utils.general import check_file, check_img_size, non_max_suppression, scale_coords, xyxy2xywh, \
+from utils.general import check_file, check_img_size, non_max_suppression, scale_coords, \
     clip_coords, plot_images, xywh2xyxy, box_iou, output_to_target, ap_per_class, attempt_load, select_device, \
     time_synchronized, compute_loss
 
 
 def test(data, weights=None, batch_size=32, imgsz=896, conf_thres=0.001, iou_thres=0.6,  # for NMS
-         save_json=False, single_cls=False, augment=False, model=None, dataloader=None, save_dir='',
-         merge=False, save_txt=False):
+         save_json=False, single_cls=False, augment=False, model=None, dataloader=None, save_dir='inference',
+         merge=False):
     # Initialize/load model and set device
     training = model is not None
     if training:  # called by train.py
@@ -26,20 +24,13 @@ def test(data, weights=None, batch_size=32, imgsz=896, conf_thres=0.001, iou_thr
 
     else:  # called directly
         device = select_device(opt.device, batch_size=batch_size)
-        merge, save_txt = opt.merge, opt.save_txt  # use Merge NMS, save *.txt labels
-        if save_txt:
-            out = Path('inference/output')
-            if os.path.exists(out):
-                shutil.rmtree(out)  # delete output folder
-            os.makedirs(out)  # make new output folder
-
-        # Remove previous
-        for f in glob.glob(str(Path(save_dir) / 'test_batch*.jpg')):
-            os.remove(f)
-
+        merge = opt.merge  # use Merge NMS
         # Load model
         model = attempt_load(weights, map_location=device)  # load FP32 model
         imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
+
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
 
     # Half
     half = device.type != 'cpu'  # half precision only supported on CUDA
@@ -104,32 +95,20 @@ def test(data, weights=None, batch_size=32, imgsz=896, conf_thres=0.001, iou_thr
                     stats.append((torch.zeros(0, niou, dtype=torch.bool), torch.Tensor(), torch.Tensor(), tcls))
                 continue
 
-            # Append to text file
-            if save_txt:
-                gn = torch.tensor(shapes[si][0])[[1, 0, 1, 0]]  # normalization gain whwh
-                txt_path = str(out / Path(paths[si]).stem)
-                pred[:, :4] = scale_coords(img[si].shape[1:], pred[:, :4], shapes[si][0], shapes[si][1])  # to original
-                for *xyxy, conf, cls in pred:
-                    xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                    with open(txt_path + '.txt', 'a') as f:
-                        f.write(('%g ' * 5 + '\n') % (cls, *xywh))  # label format
-
             # Clip boxes to image bounds
             clip_coords(pred, (height, width))
 
-            # Append to pycocotools JSON dictionary
+            # Append to JSON dictionary
             if save_json:
                 # [{"name": 42, "category": 18, "bbox": [258.15, 41.29, 348.26, 243.78], "score": 0.236}, ...
                 image_id = Path(paths[si]).stem
                 box = pred[:, :4].clone()  # xyxy
                 scale_coords(img[si].shape[1:], box, shapes[si][0], shapes[si][1])  # to original shape
-                box = xyxy2xywh(box)  # xywh
-                box[:, :2] -= box[:, 2:] / 2  # xy center to top-left corner
                 for p, b in zip(pred.tolist(), box.tolist()):
                     jdict.append({'name': '{}.jpg'.format(image_id),
                                   'category': int(p[5]),
-                                  'bbox': [round(x, 3) for x in b],
-                                  'score': round(p[4], 5)})
+                                  'bbox': [round(x, 2) for x in b],
+                                  'score': round(p[4], 6)})
 
             # Assign all predictions as incorrect
             correct = torch.zeros(pred.shape[0], niou, dtype=torch.bool, device=device)
@@ -192,9 +171,7 @@ def test(data, weights=None, batch_size=32, imgsz=896, conf_thres=0.001, iou_thr
 
     # Save JSON
     if save_json and len(jdict):
-        f = 'detections_%s_results.json' % \
-            (weights.split(os.sep)[-1].replace('.pt', '') if isinstance(weights, str) else '')  # filename
-        print('\nCOCO mAP with pycocotools... saving %s...' % f)
+        f = Path(save_dir) / 'results.json'  # filename
         with open(f, 'w') as file:
             json.dump(jdict, file, indent=4)
 
@@ -220,8 +197,7 @@ if __name__ == '__main__':
     parser.add_argument('--single-cls', action='store_true', help='treat as single-class dataset')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
     parser.add_argument('--merge', action='store_true', help='use Merge NMS')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-dir', type=str, default='inference/', help='saving directory')
+    parser.add_argument('--save-dir', type=str, default='inference', help='saving directory')
     opt = parser.parse_args()
     opt.data = check_file(opt.data)  # check file
     print(opt)
