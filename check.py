2c8a3d4bfbf8ae184546fdc886722a5ff845b27f
leftthomas <leftthomas@qq.com>
2021-01-12 20:53:39 +0800

    remove check code

--- check.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import cv2
-from tqdm import tqdm
-
-path = 'data/train/images'
-for file in tqdm(os.listdir(path)):
-    img = cv2.imread('{}/{}'.format(path, file))
-    cv2.imwrite('{}/{}'.format(path, file), img)
